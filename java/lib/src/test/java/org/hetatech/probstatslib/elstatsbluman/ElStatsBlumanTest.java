5111c43e9c03a9f4c5c655200e8801795f3dea3a
robkadj <robert.kadjinskii@gmail.com>
2023-05-29 15:59:41 +0300

    Switched to JUnit 5, removed dummy class Library and its test LibraryTest.java

--- java/lib/src/test/java/org/hetatech/probstatslib/elstatsbluman/ElStatsBlumanTest.java
+++ java/lib/src/test/java/org/hetatech/probstatslib/elstatsbluman/ElStatsBlumanTest.java
@@ -1,8 +1,8 @@
 package org.hetatech.probstatslib.elstatsbluman;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static junit.framework.TestCase.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ElStatsBlumanTest {
 
